f2ec72af58082095517fde37321cb622b6bb893f
acdicator <acdicator@eacdicator.live>
2018-01-23 11:39:57 +0700

    create static files

---
+++ static/js/chat.js
@@ -0,0 +1,80 @@
+var socket = io.connect('http://localhost:3000');  
+
+var app = new Vue({
+    el: '#app',
+
+    props: {
+        apiUrl: {
+            type: String,
+            default() {
+                return 'http://localhost:3000/api'
+            }
+        },
+        chatName: {
+            type: String,
+            default() {
+                return ''
+            }
+        }
+    },       
+
+    data() {
+        return {
+            messages: [],
+            newMessage: ''
+        }
+    },
+
+    created() {
+        this.generateChatName()
+    },
+
+    mounted () {
+        socket.on('web-messenger', (msg) => {
+            this.messages.push({
+                sender: msg.sender,
+                message: msg.message 
+            })
+        });
+
+        $('#chat-name-modal').modal({
+            backdrop: 'static',
+            keyboard: false
+        })
+    },
+
+    methods: {
+        generateChatName() {
+            this.chatName = 'u' + Math.floor(Math.random() * 10000) + 1
+        },
+
+        fetchMessages() {
+            axios.get(this.apiUrl+'/messages')
+            .then((response) => {
+                this.messages = response.data.response;
+            })
+            .catch(function(err) {
+                
+            })
+        },
+
+        confirmDisplayName() {
+            if (this.chatName.trim() != '') {
+                $('#chat-name-modal').modal('hide')
+                this.fetchMessages();
+            }
+        },
+
+        sendMessage() {
+            socket.emit('web-messenger', { 
+                sender: this.chatName,
+                message: this.newMessage 
+            })
+
+            this.newMessage = ''
+
+            var scrollTo_int = $('.chat-box').prop('scrollHeight') + 'px'
+            $('.chat-box').animate({scrollTop: scrollTo_int})
+        }
+    }
+})
